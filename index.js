96093f502ae49255c6e2f475959dd33973faf11c
Kaiichiro Ota <kigh.ota@gmail.com>
2018-01-26 12:37:55 +0900

    文字位置を指定

--- index.js
+++ index.js
@@ -10,11 +10,30 @@ const PdfPrinter = require('pdfmake/src/printer');
 const printer = new PdfPrinter(fonts);
 const fs = require('fs');
 
+function mm2pt(v) {
+  return v * 2.8346;
+}
+
 const docDefinition = {
-  pageSize: { width: 283.47, height: 419.54 }, // 100 x 148 mm
+  pageSize: { width: mm2pt(100), height: mm2pt(148) }, // 100 x 148 mm
+  pageMargins: [0, 0, 0, 0],
   content: [
-    'First paragraph',
-    'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+    {
+      text: '1',
+      absolutePosition: { x: mm2pt(10), y: mm2pt(20) },
+      fontSize: 50
+    },
+    {
+      canvas: [
+        {
+          type: 'rect',
+          x: mm2pt(10),
+          y: mm2pt(20),
+          w: 50,
+          h: 50
+        }
+      ]
+    }
   ]
 };
 const pdfDoc = printer.createPdfKitDocument(docDefinition);
